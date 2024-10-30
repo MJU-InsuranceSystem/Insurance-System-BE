6702cac11887b6338bbc40b3870016aecb4098b9
TaetaetaE01 <gogo589@naver.com>
2024-10-30 16:52:50 +0900

    [init] : project setting

---
+++ src/test/java/fourservings_fiveservings/insurance_system_be/InsuranceSystemBeApplicationTests.java
@@ -0,0 +1,13 @@
+package fourservings_fiveservings.insurance_system_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceSystemBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
