04f5c3372fe205e42c0d3c36133e8278e7d72287
koseonje <127813439+KoSeonJe@users.noreply.github.com>
2024-11-05 21:29:01 +0900

    feat : baseEntity 생성

---
+++ src/main/java/fourservings_fiveservings/insurance_system_be/common/entity/BaseEntity.java
@@ -0,0 +1,23 @@
+package fourservings_fiveservings.insurance_system_be.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+    @CreatedDate
+    @Column(columnDefinition = "TIMESTAMP", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime updatedAt;
+}
