741c4ce16003aeeab5c1c6416561ba4018c24348
0702Yoon <gkstkddbs99@naver.com>
2024-11-08 11:33:03 +0900

    Feat : product 기획 api 구현과 product 객체 구현

---
+++ src/main/java/fourservings_fiveservings/insurance_system_be/team/product/controller/ProductController.java
@@ -0,0 +1,30 @@
+package fourservings_fiveservings.insurance_system_be.team.product.controller;
+
+import fourservings_fiveservings.insurance_system_be.common.constant.SuccessType;
+import fourservings_fiveservings.insurance_system_be.common.vo.ApiResponse;
+import fourservings_fiveservings.insurance_system_be.team.product.api.ProductApi;
+import fourservings_fiveservings.insurance_system_be.team.product.controller.dto.DesignProductRequest;
+import fourservings_fiveservings.insurance_system_be.team.product.entity.Product;
+import fourservings_fiveservings.insurance_system_be.team.product.service.ProductService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ProductController implements ProductApi {
+
+    private final ProductService productService;
+
+    @Override
+    public ApiResponse<?> designProduct(DesignProductRequest designProductRequest) {
+        productService.designProduct(designProductRequest);
+        return ApiResponse.success(SuccessType.SUCCESS);
+    }
+
+    @Override
+    public ApiResponse<?> getUnapprovedProducts() {
+        List<Product> unapprovedProducts = productService.retrieveUnapprovedProducts();
+        return ApiResponse.success(SuccessType.SUCCESS, unapprovedProducts);
+    }
+}
