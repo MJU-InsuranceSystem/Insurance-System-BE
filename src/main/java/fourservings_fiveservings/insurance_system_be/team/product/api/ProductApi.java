efb0a58676f009ea45254966c896aad6984276e1
0702Yoon <gkstkddbs99@naver.com>
2024-11-10 17:47:37 +0900

    Feat : product 등록할 때 만든 사람 표시, product 승인할 때 승인 책임자 구현

--- src/main/java/fourservings_fiveservings/insurance_system_be/team/product/api/ProductApi.java
+++ src/main/java/fourservings_fiveservings/insurance_system_be/team/product/api/ProductApi.java
@@ -1,7 +1,9 @@
 package fourservings_fiveservings.insurance_system_be.team.product.api;
 
+import fourservings_fiveservings.insurance_system_be.auth.custom.CustomUserDetails;
 import fourservings_fiveservings.insurance_system_be.common.response.vo.ApiResponse;
 import fourservings_fiveservings.insurance_system_be.team.product.controller.dto.DesignProductRequest;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,11 +19,13 @@ public interface ProductApi {
     // 상품 기획안 허락하기,
     // 상품 등록하기? 흠
     @PostMapping()
-    ApiResponse<?> designProduct(@RequestBody DesignProductRequest designProductRequest);
+    ApiResponse<?> designProduct(@AuthenticationPrincipal CustomUserDetails customUserDetails,
+        @RequestBody DesignProductRequest designProductRequest);
 
     @GetMapping()
     ApiResponse<?> getUnapprovedProducts();
 
     @PatchMapping("/{productId}")
-    ApiResponse<?> approveProduct(@PathVariable(name = "productId") Long productId);
+    ApiResponse<?> approveProduct(@AuthenticationPrincipal CustomUserDetails customUserDetails,
+        @PathVariable(name = "productId") Long productId);
 }
