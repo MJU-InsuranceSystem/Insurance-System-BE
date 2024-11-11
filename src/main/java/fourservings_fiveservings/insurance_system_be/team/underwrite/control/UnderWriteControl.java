d3bfed50855211b4b26654177b153b824db38762
0702Yoon <gkstkddbs99@naver.com>
2024-11-11 15:09:18 +0900

    Refactor : 상품 승인 underwrite -> product로 변경

--- src/main/java/fourservings_fiveservings/insurance_system_be/team/underwrite/control/UnderWriteControl.java
+++ src/main/java/fourservings_fiveservings/insurance_system_be/team/underwrite/control/UnderWriteControl.java
@@ -1,9 +1,5 @@
 package fourservings_fiveservings.insurance_system_be.team.underwrite.control;
 
-import fourservings_fiveservings.insurance_system_be.auth.custom.CustomUserDetails;
-import fourservings_fiveservings.insurance_system_be.common.response.constant.SuccessType;
-import fourservings_fiveservings.insurance_system_be.common.response.vo.ApiResponse;
-import fourservings_fiveservings.insurance_system_be.domain.user.entity.User;
 import fourservings_fiveservings.insurance_system_be.team.underwrite.api.UnderwriteApi;
 import fourservings_fiveservings.insurance_system_be.team.underwrite.service.UnderWriteService;
 import lombok.RequiredArgsConstructor;
@@ -14,12 +10,5 @@ import org.springframework.web.bind.annotation.RestController;
 public class UnderWriteControl implements UnderwriteApi {
 
     private final UnderWriteService underWriteService;
-
-    @Override
-    public ApiResponse<?> approveProduct(CustomUserDetails customUserDetailsService,
-        Long productId) {
-        User underwriter = customUserDetailsService.getUser();
-        underWriteService.approveProduct(underwriter, productId);
-        return ApiResponse.success(SuccessType.SUCCESS);
-    }
+    
 }
