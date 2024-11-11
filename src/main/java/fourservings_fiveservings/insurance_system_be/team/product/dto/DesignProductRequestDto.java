bbbf3a52ac07de445bf3dcff39001c26766ffc0e
0702Yoon <gkstkddbs99@naver.com>
2024-11-11 15:09:41 +0900

    Refactor : 상품 승인 underwrite -> product로 변경

--- src/main/java/fourservings_fiveservings/insurance_system_be/team/product/dto/DesignProductRequestDto.java
+++ src/main/java/fourservings_fiveservings/insurance_system_be/team/product/dto/DesignProductRequestDto.java
@@ -1,28 +1,28 @@
 package fourservings_fiveservings.insurance_system_be.team.product.dto;
 
 
+import fourservings_fiveservings.insurance_system_be.domain.insurance.ApproveStatus;
 import fourservings_fiveservings.insurance_system_be.domain.user.entity.User;
 import fourservings_fiveservings.insurance_system_be.team.plan.design.model.SaleTarget;
-import fourservings_fiveservings.insurance_system_be.team.product.entity.ApproveStatus;
 import fourservings_fiveservings.insurance_system_be.team.product.entity.Product;
 
 public record DesignProductRequestDto(
-        // 상품 제목, 상품 개요, 판매 대상, 매달 보험료, 보상 내용, 전략,
-        String productTitle, String productOutline,
-        SaleTarget saleTarget, Integer monthlyPaymentAmount,
-        String rewardDetails, String strategy
+    // 상품 제목, 상품 개요, 판매 대상, 매달 보험료, 보상 내용, 전략,
+    String productTitle, String productOutline,
+    SaleTarget saleTarget, Integer monthlyPaymentAmount,
+    String rewardDetails, String strategy
 ) {
 
     public Product toProduct(User productDeveloper) {
         return Product.builder()
-                .productTitle(productTitle)
-                .productOutline(productOutline)
-                .saleTarget(saleTarget)
-                .monthlyPaymentAmount(monthlyPaymentAmount)
-                .rewardDetails(rewardDetails)
-                .strategy(strategy)
-                .approveStatus(ApproveStatus.UN_APPROVE)
-                .productDeveloper(productDeveloper)
-                .build();
+            .productTitle(productTitle)
+            .productOutline(productOutline)
+            .saleTarget(saleTarget)
+            .monthlyPaymentAmount(monthlyPaymentAmount)
+            .rewardDetails(rewardDetails)
+            .strategy(strategy)
+            .approveStatus(ApproveStatus.UN_APPROVE)
+            .productDeveloper(productDeveloper)
+            .build();
     }
 }
