65a769e4cc3511365d1643661e6b41098c640e09
0702Yoon <gkstkddbs99@naver.com>
2024-11-13 14:07:26 +0900

    Feat : 계약 승인 api 구현

--- src/main/java/fourservings_fiveservings/insurance_system_be/team/contract/entity/Contract.java
+++ src/main/java/fourservings_fiveservings/insurance_system_be/team/contract/entity/Contract.java
@@ -1,11 +1,22 @@
 package fourservings_fiveservings.insurance_system_be.team.contract.entity;
 
 import fourservings_fiveservings.insurance_system_be.common.entity.BaseEntity;
-import fourservings_fiveservings.insurance_system_be.team.product.insurance.ApproveStatus;
 import fourservings_fiveservings.insurance_system_be.team.product.entity.Product;
+import fourservings_fiveservings.insurance_system_be.team.product.insurance.ApproveStatus;
 import fourservings_fiveservings.insurance_system_be.user.entity.User;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
@@ -18,26 +29,33 @@ public class Contract extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @ManyToOne
-    private User user;
-
-    @ManyToOne
-    private Product product;
-
     @Embedded
     private InsuranceApplication insuranceApplication;
 
     @Enumerated(EnumType.STRING)
     private ApproveStatus approveStatus;
 
-    public static Contract fromUnApproveContract(User appliedCustomer, Product product) {
+    @ManyToOne
+    private User subscriber;
+
+    @ManyToOne
+    private Product product;
+
+    @ManyToOne
+    private User underwriteManager;
+
+    public static Contract fromUnApproveContract(User subscriber, Product product) {
         return Contract.builder()
-            .user(appliedCustomer)
+            .subscriber(subscriber)
             .product(product)
             .approveStatus(ApproveStatus.UN_APPROVE)
             .build();
     }
 
+    public void approveContract(User underwriteManager) {
+        this.approveStatus = ApproveStatus.APPROVE;
+        this.underwriteManager = underwriteManager;
+    }
 
     //    @Override
 //    public String toString() {
