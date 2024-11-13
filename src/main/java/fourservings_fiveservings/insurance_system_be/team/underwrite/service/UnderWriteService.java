b18d107cce31e776b9ef9e80f965133e49ac3002
0702Yoon <gkstkddbs99@naver.com>
2024-11-13 14:07:18 +0900

    Feat : 계약 승인 api 구현

--- src/main/java/fourservings_fiveservings/insurance_system_be/team/underwrite/service/UnderWriteService.java
+++ src/main/java/fourservings_fiveservings/insurance_system_be/team/underwrite/service/UnderWriteService.java
@@ -1,5 +1,9 @@
 package fourservings_fiveservings.insurance_system_be.team.underwrite.service;
 
+import fourservings_fiveservings.insurance_system_be.team.contract.entity.Contract;
+import fourservings_fiveservings.insurance_system_be.team.contract.repository.ContractRepository;
+import fourservings_fiveservings.insurance_system_be.user.entity.User;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,5 +12,13 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class UnderWriteService {
-    
+
+    private final ContractRepository contractRepository;
+
+    @Transactional
+    public void approveContract(User underwriteManager, Integer contractId) {
+        Contract contract = contractRepository.findById(contractId).orElseThrow(
+            EntityNotFoundException::new);
+        contract.approveContract(underwriteManager);
+    }
 }
