d4e0718c9227e5da4acdae2551c7cd294a192b8d
TaetaetaE01 <gogo589@naver.com>
2024-11-18 19:28:17 +0900

    feat : 고객 계좌 필드 생성

--- src/main/java/fourservings_fiveservings/insurance_system_be/user/entity/Customer.java
+++ src/main/java/fourservings_fiveservings/insurance_system_be/user/entity/Customer.java
@@ -1,12 +1,10 @@
 package fourservings_fiveservings.insurance_system_be.user.entity;
 
-import fourservings_fiveservings.insurance_system_be.user.entity.embeded.ContractStatus;
+import fourservings_fiveservings.insurance_system_be.user.entity.embeded.Account;
 import fourservings_fiveservings.insurance_system_be.user.entity.embeded.Address;
-import fourservings_fiveservings.insurance_system_be.user.entity.embeded.UserType;
-import jakarta.persistence.DiscriminatorValue;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import fourservings_fiveservings.insurance_system_be.user.entity.enums.ContractStatus;
+import fourservings_fiveservings.insurance_system_be.user.entity.enums.UserType;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,6 +15,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @DiscriminatorValue("CUSTOMER")
 public class Customer extends User {
+
+    @Embedded
+    private Account account;
 
     @Enumerated(EnumType.STRING)
     private ContractStatus contractStatus;
