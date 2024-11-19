a5c0e9d9474f8db8b00ef34dd954c6a962566ab5
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2024-11-19 17:33:29 +0900

    refactor : 필요없는 파일 제거

--- src/main/java/fourservings_fiveservings/insurance_system_be/team/plan/design/model/SaleTarget.java
+++
@@ -1,5 +0,0 @@
-package fourservings_fiveservings.insurance_system_be.team.plan.design.model;
-
-public enum SaleTarget {
-    TEENAGER, TWO_TEENAGER;
-}
--- src/main/java/fourservings_fiveservings/insurance_system_be/team/product/insurance/type/CarKind.java
+++
@@ -1,4 +0,0 @@
-package fourservings_fiveservings.insurance_system_be.team.product.insurance.type;
-
-public enum CarKind {
-}
