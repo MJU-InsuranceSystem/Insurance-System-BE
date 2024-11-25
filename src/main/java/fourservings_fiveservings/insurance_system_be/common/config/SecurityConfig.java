031ac3b927827a136dc8fce88fe1f911a55e7ad5
TaetaetaE01 <gogo589@naver.com>
2024-11-25 14:44:00 +0900

    refactor : cors 에러 front url 명시

--- src/main/java/fourservings_fiveservings/insurance_system_be/common/config/SecurityConfig.java
+++ src/main/java/fourservings_fiveservings/insurance_system_be/common/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import fourservings_fiveservings.insurance_system_be.auth.jwt.service.JwtService
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -35,47 +36,54 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http,
-        HandlerMappingIntrospector introspector) throws Exception {
+                                           HandlerMappingIntrospector introspector) throws Exception {
         http
-            .csrf(AbstractHttpConfigurer::disable)
-            .formLogin(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
 
-            //http basic 인증 방식 disable
-            .httpBasic(AbstractHttpConfigurer::disable)
+                //http basic 인증 방식 disable
+                .httpBasic(AbstractHttpConfigurer::disable)
 
-            // 세션 생성하지 않음
-            .sessionManagement(sessionManagement ->
-                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // 세션 생성하지 않음
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
-            // 인증되지 않은 요청에 대해 401에러 -> exception 커스텀 가능
-            .exceptionHandling(e -> e
-                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+                // 인증되지 않은 요청에 대해 401에러 -> exception 커스텀 가능
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
 
-            /**
-             * 아래 url은 인증 없이 접근 가능
-             */
-            .authorizeHttpRequests(authorizeHttpRequests ->
-                authorizeHttpRequests
-                    .requestMatchers(new MvcRequestMatcher(introspector, "/api/auth/sign-up"))
-                    .permitAll()
-                    .requestMatchers(new MvcRequestMatcher(introspector, "/api/auth/sign-in"))
-                    .permitAll()
-                    .requestMatchers(new MvcRequestMatcher(introspector, "/api/auth/**"))
-                    .permitAll()
-                    .anyRequest().authenticated()) // 그 외는 접근x
-            .cors(cors -> cors
-                .configurationSource(corsConfigurationSource())
-            )
+                /**
+                 * 아래 url은 인증 없이 접근 가능
+                 */
+                .authorizeHttpRequests(authorizeHttpRequests ->
+                        authorizeHttpRequests
+                                .requestMatchers(new MvcRequestMatcher(introspector, "/api/auth/sign-up"))
+                                .permitAll()
+                                .requestMatchers(new MvcRequestMatcher(introspector, "/api/auth/sign-in"))
+                                .permitAll()
+                                .requestMatchers(new MvcRequestMatcher(introspector, "/api/auth/**"))
+                                .permitAll()
+                                .requestMatchers(HttpMethod.OPTIONS, "/**")
+                                .permitAll()
+                                .anyRequest().authenticated()) // 그 외는 접근x
+                .cors(cors -> cors
+                        .configurationSource(corsConfigurationSource())
+                )
 
-            // jwtFilter 후 UsernamePasswordAuthenticationFilter 인증 처리
-            .addFilterBefore(new JwtAuthenticationFilter(jwtService),
-                UsernamePasswordAuthenticationFilter.class);
+                // jwtFilter 후 UsernamePasswordAuthenticationFilter 인증 처리
+                .addFilterBefore(new JwtAuthenticationFilter(jwtService),
+                        UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
 
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOrigin("https://insurance-system-fe.vercel.app");
+        configuration.addAllowedOrigin("https://insurance-system-fe-git-main-kimwlyus-projects.vercel.app");
+        configuration.addAllowedOrigin("https://insurance-system-pr21ai5ml-kimwlyus-projects.vercel.app");
+        configuration.addAllowedOrigin("http://localhost:3000");
 
         configuration.addAllowedOriginPattern("*");
         configuration.addAllowedHeader("*");
