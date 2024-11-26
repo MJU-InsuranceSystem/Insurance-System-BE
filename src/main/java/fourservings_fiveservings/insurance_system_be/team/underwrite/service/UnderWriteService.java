431aaaa3b61c9f0d291ec687805cb83b6e546a75
0702Yoon <gkstkddbs99@naver.com>
2024-11-26 14:56:53 +0900

    refactor : User가 아닌 Customer, Worker의 구현체로 정의

--- src/main/java/fourservings_fiveservings/insurance_system_be/team/underwrite/service/UnderWriteService.java
+++ src/main/java/fourservings_fiveservings/insurance_system_be/team/underwrite/service/UnderWriteService.java
@@ -3,7 +3,7 @@ package fourservings_fiveservings.insurance_system_be.team.underwrite.service;
 import fourservings_fiveservings.insurance_system_be.team.contract.common.entity.Contract;
 import fourservings_fiveservings.insurance_system_be.team.contract.common.repository.ContractRepository;
 import fourservings_fiveservings.insurance_system_be.team.underwrite.controller.dto.request.UnderwriteRequestDto;
-import fourservings_fiveservings.insurance_system_be.user.entity.User;
+import fourservings_fiveservings.insurance_system_be.user.entity.Worker;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,7 +17,7 @@ public class UnderWriteService {
     private final ContractRepository contractRepository;
 
     @Transactional
-    public void approveContract(User underwriteManager, Long contractId,
+    public void approveContract(Worker underwriteManager, Long contractId,
         UnderwriteRequestDto underwriteRequestDto) {
         Contract contract = contractRepository.findById(contractId).orElseThrow(
             EntityNotFoundException::new);
